68a62a3c80dfb319a5e337d019a4983a60199d9d
Michael Fehrenbach <mf@michaelfehrenbach.com>
2026-06-01 15:17:19 -0400

    Add base layout

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,8 +1,12 @@
 export default (eleventyConfig) => {
+	eleventyConfig.addGlobalData('layout', 'base.liquid')
 	return {
 		dir: {
 			input: 'content',
 			output: '_site',
+
+			// Relative to `input`.
+			layouts: '../layouts',
 		}
 	}
 }
