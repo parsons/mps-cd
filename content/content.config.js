12193b9f6d78f49168f57dc31e1010a17212e2d2
Michael Fehrenbach <mf@michaelfehrenbach.com>
2026-06-02 11:20:31 -0400

    Naïve nav

--- content/content.config.js
+++ content/content.config.js
@@ -1,6 +1,7 @@
 export default {
 	eleventyComputed: {
 		programTitle: 'Parsons MPS Communication Design',
+		tags: data => data.page.url !== '/' && ['page'], // Make a collection of sub-pages.
 		title: data => data.page.fileSlug // Use the folder name…
 			.replace(/^./, initial => initial.toUpperCase()) // …capitalized.
 			|| data.programTitle // Fallback to program.
