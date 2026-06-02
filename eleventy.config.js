3262d1e0333d3f818b15ddcdee10ffa291d1473c
Michael Fehrenbach <mf@michaelfehrenbach.com>
2026-06-02 14:16:58 -0400

    Fix root URL

--- eleventy.config.js
+++ eleventy.config.js
@@ -6,7 +6,8 @@ export default (eleventyConfig) => {
 
 	// Override filter with relative URLs, for portability. (Using regular function for `this`.)
 	eleventyConfig.addFilter('url', function (target) {
-		return path.join(path.relative(this.page.url, target), target.endsWith('/') ? '/' : '')
+		const result = path.relative(this.page.url, target) || '.'
+		return target.endsWith('/') ? `${result}/` : result
 	})
 
 	return {
