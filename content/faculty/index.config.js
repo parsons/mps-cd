7a685a75c62ce67a7dade16ca5fdbbca75d06c95
Michael Fehrenbach <mf@michaelfehrenbach.com>
2026-06-02 17:01:32 -0400

    Previous faculty

--- content/faculty/index.config.js
+++ content/faculty/index.config.js
@@ -56,12 +56,154 @@ export default {
 			{
 				first: 'Mianwei',
 				last:  'Wang',
-				url:   'https://mianweiwang.com/',
+				url:   'https://mianweiwang.com',
 			},
 			{
 				first: 'Rijk',
 				last:  'van Zanten',
 				url:   'https://rijk.nyc',
+			},
+		],
+		previous: [
+			{
+				first: 'Chuck Sarah',
+				last:  'Allison',
+				url:   'https://www.linkedin.com/in/janet-sarah-allison',
+			},
+			{
+				first: 'Jonathan',
+				last:  'Lee',
+				url:   'https://www.linkedin.com/in/hifromjonathan',
+			},
+			{
+				first: 'Crayon',
+				last:  'Hsieh',
+				url:   'https://imcrayon.com',
+			},
+			{
+				first: 'Abby',
+				last:  'Ricarte',
+				url:   'http://abbyricarte.com',
+			},
+			{
+				first: 'Brendan',
+				last:  'Griffiths',
+				url:   'https://brendangriffiths.com',
+			},
+			{
+				first: 'Lynn',
+				last:  'Kiang',
+				url:   'https://www.domecollective.com',
+			},
+			{
+				first: 'Luiza',
+				last:  'Dale',
+				url:   'http://luizadale.com',
+			},
+			{
+				first: 'Laura',
+				last:  'Tolomelli',
+				url:   'http://laura-tolomelli.com',
+			},
+			{
+				first: 'Rocco',
+				last:  'Rada',
+				url:   'https://www.roccorada.com',
+			},
+			{
+				first: 'Jacob',
+				last:  'Heftmann',
+				url:   'http://www.jacobheftmann.com',
+			},
+			{
+				first: 'Tuan',
+				last:  'Quoc Pham',
+				url:   'https://tuanpham.info',
+			},
+			{
+				first: 'Lucille',
+				last:  'Tenazas',
+				url:   'https://en.wikipedia.org/wiki/Lucille_Tenazas',
+			},
+			{
+				first: 'Nana',
+				last:  'Marin',
+				url:   'https://www.nanamarin.com',
+			},
+			{
+				first: 'Kimberly',
+				last:  'Gim',
+				url:   'https://www.linkedin.com/in/kimberly-gim',
+			},
+			{
+				first: 'Carly',
+				last:  'Ayres',
+				url:   'http://carlyayres.com',
+			},
+			{
+				first: 'Evan',
+				last:  'Chang',
+				url:   'http://evanchang.work',
+			},
+			{
+				first: 'E Roon',
+				last:  'Kang',
+				url:   'http://eroonkang.com',
+			},
+			{
+				first: 'Conor',
+				last:  'McGlauflin',
+				url:   'http://conormcglauflin.com',
+			},
+			{
+				first: 'Sebastian',
+				last:  'Speier',
+				url:   'http://sspeier.com',
+			},
+			{
+				first: 'Robert',
+				last:  'Spica',
+				url:   'https://www.linkedin.com/in/robertspica',
+			},
+			{
+				first: 'Lukas Eigler',
+				last:  'Harding',
+				url:   'http://lukas.eigler-harding.com',
+			},
+			{
+				first: 'Krystyn',
+				last:  'Heide',
+				url:   'https://makethingsbetter.studio',
+			},
+			{
+				first: 'Erica',
+				last:  'Heinz',
+				url:   'http://ericaheinz.com',
+			},
+			{
+				first: 'Tiff',
+				last:  'Hockin',
+				url:   'https://tiffhockin.info',
+			},
+			{
+				first: 'Nick',
+				last:  'Schaden',
+				url:   'http://www.nickschaden.com',
+			},
+			{
+				first: 'Rahul',
+				last:  'Shinde',
+				url:   'https://rahulshinde.com',
+			},
+			{
+				first: 'Kelly',
+				last:  'Walters',
+				url:   'https://brightpolkadot.com',
+			},
+			{
+				first: 'Brian',
+				last:  'Watterson',
+				url:   'https://brianwatterson.com',
 			},
 		]
 	}
