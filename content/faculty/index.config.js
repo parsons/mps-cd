8ecb5491c1f695b4af043c6e51e666d6a35a5927
Michael Fehrenbach <mf@michaelfehrenbach.com>
2026-06-02 16:30:21 -0400

    Current faculty list

--- content/faculty/index.config.js
+++ content/faculty/index.config.js
@@ -1,3 +1,68 @@
 export default {
 	order: 2,
+
+	faculty: {
+		current: [
+			{
+				first: 'Adam',
+				last:  'Brodowski',
+				url:   'https://abrodowski.com',
+			},
+			{
+				first: 'Clara',
+				last:  'Bunker',
+				url:   'https://clarajoybunker.com',
+			},
+			{
+				first: 'Juliette',
+				last:  'Cezzar',
+				url:   'https://juliettecezzar.com',
+			},
+			{
+				first: 'Sally',
+				last:  'Chung',
+				url:   'https://www.sallyhychung.com',
+			},
+			{
+				first: 'Michael',
+				last:  'Fehrenbach',
+				url:   'https://michaelfehrenbach.com',
+			},
+			{
+				first: 'Dana',
+				last:  'Jefferson',
+				url:   'https://danajefferson.com',
+			},
+			{
+				first: 'Andrew',
+				last:  'LeClair',
+				url:   'https://andrewleclair.com',
+			},
+			{
+				first: 'Eric',
+				last:  'Li',
+				url:   'https://eric.young.li',
+			},
+			{
+				first: 'Amanda',
+				last:  'Pinsker',
+				url:   'https://amandapinsker.com',
+			},
+			{
+				first: 'Lauren',
+				last:  'Schlanger',
+				url:   'https://www.linkedin.com/in/schlanger',
+			},
+			{
+				first: 'Mianwei',
+				last:  'Wang',
+				url:   'https://mianweiwang.com/',
+			},
+			{
+				first: 'Rijk',
+				last:  'van Zanten',
+				url:   'https://rijk.nyc',
+			},
+		]
+	}
 }
