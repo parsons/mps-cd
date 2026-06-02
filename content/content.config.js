e0b0e9d0fbcbc1a3962486214c7058aa61cb388b
Michael Fehrenbach <mf@michaelfehrenbach.com>
2026-06-02 12:03:06 -0400

    Better nav

--- content/content.config.js
+++ content/content.config.js
@@ -1,6 +1,11 @@
 export default {
+	school:        'Parsons',
+	program:       'MPS Communication Design',
+	concentration: 'Digital Product Design',
+
+	// Derived data.
 	eleventyComputed: {
-		programTitle: 'Parsons MPS Communication Design',
+		programTitle: data => `${data.school} ${data.program}`,
 		tags: data => data.page.url !== '/' && ['page'], // Make a collection of sub-pages.
 		title: data => data.page.fileSlug // Use the folder name…
 			.replace(/^./, initial => initial.toUpperCase()) // …capitalized.
