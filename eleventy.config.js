d2c737e4ded7dab119523a33dc3cff7e73a29486
Michael Fehrenbach <mf@michaelfehrenbach.com>
2026-06-02 14:13:05 -0400

    Relative URLs

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,13 @@
+import path from 'node:path'
+
 export default (eleventyConfig) => {
 	eleventyConfig.addGlobalData('layout', 'base.liquid') // Set layout for everything.
 	eleventyConfig.setDataFileSuffixes(['.config']) // Cosmetic, ex: `content.config.js`.
+
+	// Override filter with relative URLs, for portability. (Using regular function for `this`.)
+	eleventyConfig.addFilter('url', function (target) {
+		return path.join(path.relative(this.page.url, target), target.endsWith('/') ? '/' : '')
+	})
 
 	return {
 		dir: {
