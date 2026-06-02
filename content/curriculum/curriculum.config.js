e83b122cac5594dfd53e7c10d242ac2d715f61d7
Michael Fehrenbach <mf@michaelfehrenbach.com>
2026-06-02 15:48:50 -0400

    Curriculum table from data

--- content/curriculum/curriculum.config.js
+++ content/curriculum/curriculum.config.js
@@ -1,3 +1,28 @@
 export default {
-	order: 1
+	order: 1, // In navigation.
+
+	courseCatalog: 'https://courses.newschool.edu/courses/',
+	curriculum: {
+		fall: [
+			{
+				number:  'PMCD 5101',
+				credits:  6,
+				title:   'Major Studio 1',
+			},
+			{
+				number:  'PMCD 5001',
+				credits:  3,
+				title:   'Typography and Interaction 1',
+			},
+			{
+				number:  'PMCD 5200',
+				credits:  3,
+				title:   'Visual Culture Seminar',
+			},
+			{
+				credits:  3,
+				title:   'Elective',
+			},
+		],
+	}
 }
