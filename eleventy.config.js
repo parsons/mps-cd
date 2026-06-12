3d88e4fed0a424b5ef1ccff43c4c7fc02dac9939
Michael Fehrenbach <mf@michaelfehrenbach.com>
2026-06-12 14:26:19 -0400

    Update config

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,9 +1,12 @@
+import yaml from 'js-yaml'
 import path from 'node:path'
 
 export default (eleventyConfig) => {
 	// Set common base layout for everything.
 	eleventyConfig.addGlobalData('layout', 'base.liquid')
 
+	// More ergonomic data.
+	eleventyConfig.addDataExtension('yaml', contents => yaml.load(contents))
 
 	// Override filter with relative URLs, for portability. (Using regular function for `this`.)
 	eleventyConfig.addFilter('url', function (target) {
