2e046899bac8ed8d4f6b53f7cc43b37750077fb6
Michael Fehrenbach <mf@michaelfehrenbach.com>
2026-06-15 18:46:18 -0400

    Don’t add empty objects

--- data/eleventyComputed.js
+++ data/eleventyComputed.js
@@ -23,10 +23,10 @@ export default {
 		Object.entries(promoteIndex(students))
 			.map(([year, studentsByName]) => [
 				year,
-				Object.entries(studentsByName).map(([name, student]) => ({
-					...student,
-					project: { ...student.project, images: imagesFor(year, name) } // Add list of images.
-				}))
+				Object.entries(studentsByName).map(([name, student]) => {
+					const images = imagesFor(year, name)
+					return images.length ? { ...student, project: { ...student.project, images } } : student // Add list of images, if there are any.
+				})
 			])
 			.sort(([a], [b]) => b - a), // Descending years.
 
