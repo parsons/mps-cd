c4cd4bf0aeb2c92ea793485a9432dd8af1b49afe
Michael Fehrenbach <mf@michaelfehrenbach.com>
2026-06-20 12:03:37 -0400

    Some YAML guardrails

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,11 @@
 import { defineConfig, includeIgnoreFile } from 'eslint/config'
 import { fileURLToPath } from 'node:url'
+import eslintPluginYml from 'eslint-plugin-yml'
 
 export default defineConfig([
 	// Use `.gitignore`.
 	includeIgnoreFile(fileURLToPath(new URL('.gitignore', import.meta.url))),
+	...eslintPluginYml.configs.recommended,
 	{
 		rules: {
 			'arrow-parens': ['error', 'always'],
@@ -13,6 +15,16 @@ export default defineConfig([
 			'prefer-const': 'error',
 			'quotes': ['error', 'single'],
 			'semi': ['error', 'never'],
+		},
+	},
+	{
+		files: ['**/*.yml', '**/*.yaml'],
+		rules: {
+			'yml/file-extension': ['error', { extension: 'yaml', caseSensitive: true }],
+			'yml/indent': ['error', 2, { indentBlockSequences: true }],
+			'yml/no-irregular-whitespace': 'off',
+			'yml/quotes': ['error', { prefer: 'single', avoidEscape: true }],
+			'yml/no-multiple-empty-lines': ['error', { max: 1, maxEOF: 0, maxBOF: 0 }],
 		},
 	},
 ])
